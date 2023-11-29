5308519bb4ef542f08c3a6cf194a75652e20bb1e
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-11-29 19:46:13 +0900

    モデルファイル実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :mail, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/, message: "should be in the format example@domain.com" }
+  validates :password, presence: true,length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "must contain at least one letter and one digit" }
+  validates_confirmation_of :password
 end
