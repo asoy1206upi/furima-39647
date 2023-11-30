2dd8bbf9afc4d6d5924370dce356322baf8d43d7
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-11-30 21:03:49 +0900

    1130ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller? 
 
   
   private
-
-  def basic_auth
+  def configure_permitted_parameters
     authenticate_or_request_with_http_basic do |username, password|
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])  
       username == 'admin' && password == '2222'
     end
   end
