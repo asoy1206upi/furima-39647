759d67c86954757f4fe4a9412c1dd41a260878ff
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-12-05 19:23:59 +0900

    商品出品機能記述

--- Gemfile
+++ Gemfile
@@ -79,3 +79,4 @@ group :development do
   gem 'rubocop', require: false
 end
 gem 'devise'
+  gem 'active_hash'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'items/new', to: 'items#new'
+  # get 'login', to: 'sessions#new'
+  # post 'login', to: 'sessions#create'
+  # delete 'logout', to: 'sessions#destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
