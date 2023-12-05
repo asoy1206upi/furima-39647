7547dcd73c6e075ec4a2b2e4ff74dd048f5c09fb
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-12-05 21:14:18 +0900

    1205商品出品機能２

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true
 end
