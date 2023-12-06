0646015e613391a1fc0d2aae3a505c72eb2f2670
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-12-07 04:51:44 +0900

    販売手数料、利益

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,17 +1,17 @@
 const priceInput = document.getElementById("item-price");
 priceInput.addEventListener("input", () => {
-  const inputValue = parseInt(priceInput.value); // 入力された価格を整数に変換
+  const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
   const profitDom = document.getElementById("profit");
   
-  if (!isNaN(inputValue)) { // 入力値が数値であることを確認
+  // 価格が入力されている場合に手数料と利益を計算して表示する
+  if (inputValue) {
     const tax = Math.floor(inputValue * 0.1); // 価格の10%を手数料とする
-    addTaxDom.innerHTML = tax.toLocaleString(); // 手数料を表示
-    
     const profit = inputValue - tax; // 利益を計算
+    addTaxDom.innerHTML = tax.toLocaleString(); // 手数料を表示
     profitDom.innerHTML = profit.toLocaleString(); // 利益を表示
   } else {
-    addTaxDom.innerHTML = "0"; // 価格が入力されていない場合は手数料をクリアする
-    profitDom.innerHTML = "0"; // 利益もクリア
+    addTaxDom.innerHTML = ""; // 価格が入力されていない場合は手数料をクリアする
+    profitDom.innerHTML = ""; // 価格が入力されていない場合は利益をクリアする
   }
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,16 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :description, presence: true
-  has_one_attached :image
+
+  #validates :name, :price, :description, :category_id, :product_condition_id, :prefecture_id, presence: true
+  #validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+  #validates :name, presence: true
+  #validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  #validates :description, presence: true
+  #validates :content, presence: true
+
+  #belongs_to :user
+  #has_one :item_image
+  #has_one_attached :image
 end
