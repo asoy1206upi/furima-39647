c9a026263f7635c772cc97924a17c679c0ef0793
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-12-07 20:13:58 +0900

    エラーメッセージ出力

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, :description, :category_id, :product_condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
-  validates :description, presence: true
 
   #validates :name, :price, :description, :category_id, :product_condition_id, :prefecture_id, presence: true
   #validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
@@ -10,8 +12,5 @@ class Item < ApplicationRecord
   #validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   #validates :description, presence: true
   #validates :content, presence: true
-
-  #belongs_to :user
-  #has_one :item_image
-  #has_one_attached :image
+  
 end
