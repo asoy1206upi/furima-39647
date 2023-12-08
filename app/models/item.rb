c6c987e03e7dd9ba6b7736fde00585513f07a67c
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-12-08 18:56:16 +0900

    コードレビュー修正４

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  include ActiveHash::Associations
+  belongs_to :category_id
+  belongs_to :product_condition_id
+  belongs_to :shipping_cost_id
+  belongs_to :prefecture_id
+  belongs_to :days_to_ship_id
+
   validates :image, presence: true
   validates :name, :description, :category_id, :product_condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
