e242f4a13488be289c237eb95a07eff5c4b2e1e5
asoy1206upi <143594148+asoy1206upi@users.noreply.github.com>
2023-12-11 19:58:53 +0900

    コードレビュー修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :find_item, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -34,11 +35,11 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    if current_user == @item.user && @item.destroy
-      redirect_to root_path, notice: '商品を削除しました'
-    else
-      redirect_to root_path, alert: '商品の削除に失敗しました'
+    if current_user == @item.user 
+      @item.destroy
     end
+
+    redirect_to root_path, alert: '商品の削除に失敗しました'
   end
 
   private
@@ -46,6 +47,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :description, :category_id, :product_condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id, :image)
   end
+  
   def find_item
     @item = Item.find(params[:id])
   end
